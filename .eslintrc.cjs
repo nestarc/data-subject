f48e92449e0b47a400ffd869888017643ac1482b
ksyq12 <ksyq12@daum.net>
2026-04-14 22:50:33 +0900

    chore: initial project bootstrap

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: { node: true, jest: true, es2022: true },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
